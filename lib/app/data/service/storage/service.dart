91788cb2e84ee68458fd15b1abe70bb90afc4b28
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2023-08-06 21:58:08 +0800

    storage

---
+++ lib/app/data/service/storage/service.dart
@@ -0,0 +1,25 @@
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:to_do_list/app/core/utils/keys.dart';
+
+class StorageService extends GetxService {
+  late GetStorage _box;
+
+  Future<StorageService> init() async {
+    _box = GetStorage();
+    // if data is not null, skip the code line below
+    // if null, write the task key as a empty array
+    await _box.writeIfNull(taskKey, []);
+    return this;
+  }
+
+  // read data from local storage
+  // T is a generic type, whether string or list of type from write if null above method
+  T read<T>(String key) {
+    return _box.read(key);
+  }
+
+  void write(String key, dynamic value) async {
+    await _box.write(key, value);
+  }
+}
